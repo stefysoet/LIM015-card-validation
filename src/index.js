f43cde7cfb7558558a272efb5323724ba162983c
stefysoet <stefysoet@hotmail.com>
2021-06-30 14:25:11 -0500

    subida de archivos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+function numeros(e) {
+    if (e.keyCode < 48 || e.keyCode >57) {
+        e.returnValue = false;
+    }
+}
+
+function val() {
+    const numero = document.getElementById('numero').value;
+    const validador = validator.isValid(numero);
+    const mascara = validator.maskify(numero);
+
+    document.getElementById('valor').innerHTML = validador ? 'TARJETA VÁLIDA' : 'TARJETA INVÁLIDA';
+    document.getElementById('maskify').innerHTML = mascara;
+}
+
+document.getElementById('numero').addEventListener('keypress', (e) => { numeros(e); });
+document.getElementById('validar').addEventListener('click', () => { val() });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
   // ...
+
+  isValid(numero){
+
+    const tarjetaArray = Array.from(numero, (x) => parseInt(x));
+
+    const reverse = tarjetaArray.reverse();
+
+    let suma = 0;
+
+    for (let i = 0; i < tarjetaArray.length; i++) {
+      let element = reverse[i];
+      if (i % 2 !== 0) {
+        element *= 2;
+      if (element > 9) {
+          element -= 9; 
+      }
+      }
+      suma += element;
+}
+
+      if (suma % 10 == 0) {
+      return true;
+}
+
+      return false;
+
+},
+
+
+  maskify:function(cardNumber){
+    return cardNumber.replace(/.(?=.{4})/g, "#");
+  
+}
+
 };
 
 export default validator;
